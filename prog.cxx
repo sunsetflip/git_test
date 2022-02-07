d79d6ba42d85df38af0c40c49d313b2dc61bb546
Zach Easterbrook <Zach.Easterbrook@purolator.com>
2022-02-06 22:50:56 -0500

    Added important missing messages

--- prog.cxx
+++ prog.cxx
@@ -5,5 +5,8 @@ using namespace std;
 int main()
 {
     cout << "Wow I'm using some git. That's crazy" << endl;
+    cout << "~~ Uguu just thought I'd add some more text." << endl;
+    cout << endl;
+    cout << "You can never have too many print statements~! Uguu~~~" << endl;
     return 0;
 }
