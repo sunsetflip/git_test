2b33f16f8d906bc25c213db472d1875be38c97bf
Zach Easterbrook <Zach.Easterbrook@purolator.com>
2022-02-07 18:39:44 -0500

    Fixed the language in the program

--- prog.cxx
+++ prog.cxx
@@ -5,8 +5,6 @@ using namespace std;
 int main()
 {
     cout << "Wow I'm using some git. That's crazy" << endl;
-    cout << "~~ Uguu just thought I'd add some more text." << endl;
-    cout << endl;
-    cout << "You can never have too many print statements~! Uguu~~~" << endl;
+    cout << "Now we are using the Queen's English in the proper manner" << endl;
     return 0;
 }
