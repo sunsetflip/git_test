da364810527f1305e90362b6beada50a62361a24
Faye Pawlowski <fayepawlowski@gmail.com>
2022-02-07 19:22:25 -0500

    Added a line of text

--- prog.cxx
+++ prog.cxx
@@ -6,5 +6,6 @@ int main()
 {
     cout << "Wow I'm using some git. That's crazy" << endl;
     cout << "Now we are using the Queen's English in the proper manner" << endl;
+    cout << "These pretzels are making me thirsty" << endl;
     return 0;
 }
