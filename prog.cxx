251cd3f2325407c0b345ccd805ecc3281ef952a0
Faye Pawlowski <fayepawlowski@gmail.com>
2023-01-11 16:04:01 -0500

    Just updating and testing something.

--- prog.cxx
+++ prog.cxx
@@ -6,7 +6,7 @@ int main()
 {
     cout << "Wow I'm using some git. That's crazy" << endl;
     cout << "Now we are using the Queen's English in the proper manner" << endl;
-    cout << "These pretzels are making me thirsty" << endl;
+    cout << "These pretzels ... are making me thirsty!" << endl;
 
     for (int i = 1; i <= 10; i++)
         cout << (i * i) << endl;
